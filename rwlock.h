ee61c029d765c540068cde140d8f06bcbc9f3dff
Evan Goldsmith <evangoldsmith1@gmail.com>
2024-04-17 23:01:02 -0400

    Add rwlock files and zemaphore.h for mac dev

---
+++ rwlock.h
@@ -0,0 +1,18 @@
+#ifndef __rwlock_h__
+#define __rwlock_h__
+
+#include <semaphore.h>
+
+typedef struct _rwlock_t {
+    sem_t writelock;
+    sem_t lock;
+    int readers;
+} rwlock_t;
+
+void rwlock_init(rwlock_t *lock);
+void rwlock_acquire_readlock(rwlock_t *lock);
+void rwlock_release_readlock(rwlock_t *lock);
+void rwlock_acquire_writelock(rwlock_t *lock);
+void rwlock_release_writelock(rwlock_t *lock);
+
+#endif /* __rwlock_h__ */
